ad8ba5198e0099182c28f90b517e6703c955ed02
Ty Richards <me@tyrichards.com>
2014-01-19 11:07:47 -0600

    Initial Install, Set Git Ignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyInsuranceSucks::Application.config.secret_key_base = '596953310df7480d02b14a71cac57c2742ed341013d302a7983e30c8f39bc0c21d204ad7c40c32a0bce35b7d60ea091d9a36bd3c523853009fa2cc443c00d1b2'
