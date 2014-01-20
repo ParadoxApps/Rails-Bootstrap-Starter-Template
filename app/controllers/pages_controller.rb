f81c8d706b32897531320b13420428b3c95874e0
Ty Richards <me@tyrichards.com>
2014-01-20 15:24:07 -0600

    Bootstrap website template complete - ready to template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def index
   end
 
-  def start
+  def about
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MyInsuranceSucks::Application.routes.draw do
 
   root "pages#index"
-  get "start" => "pages#start"
+  get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
