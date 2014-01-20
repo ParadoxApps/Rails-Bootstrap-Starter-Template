5daf072ab19ea0df101941b159454208f68fa87c
Ty Richards <me@tyrichards.com>
2014-01-20 15:01:54 -0600

    setup bootstrap js, install bootstrap template page for index, setup secondary page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 MyInsuranceSucks::Application.routes.draw do
 
   root "pages#index"
+  get "start" => "pages#start"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
